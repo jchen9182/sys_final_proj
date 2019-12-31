8a42f3e929c732cd1884f3942e58ac6ca2d3cb36
Kevin Li <kevinli4321@gmail.com>
2019-12-31 12:40:40 -0500

    File operations (untested)

---
+++ fileops.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+
+//Looking for the properties of a file? Check out fileinfo.c.
+
+void new_file() {
+  int fd = open("Untitled Document", O_CREAT | O_EXCL, 0666);
+
+  if (fd < 0)
+    fd = open("Untitled Document 2", O_CREAT, 0666);
+
+  printf("File created! \n");
+}
+
+void new_folder() {
+  int folder = mkdir("New Folder", 0755);
+
+  if (folder < 0)
+    printf("Error %d: %s", errno, strerror(errno));
+}
+
+void rename_thing(char *oldname, char *newname) {
+  int status = rename(oldname, newname);
+  if (status < 0)
+    printf("Error %d: %s", errno, strerror(errno));
+}
+
+void remove_thing(char *thingname) {
+  int status = remove(thingname);
+  if (status < 0)
+    printf("Error %d: %s", errno, strerror(errno));
+}
---
+++ fileops.h
@@ -0,0 +1,7 @@
+void new_file();
+
+void new_folder();
+
+void rename_thing(char *oldname, char *newname);
+
+void remove_thing(char *thingname);
