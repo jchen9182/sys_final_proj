92422b0c99d87f37dec206065f05bbef37610f88
Kevin Li <kevinli4321@gmail.com>
2020-01-05 19:08:07 -0500

    Kind of a grid layout, need to figure out how to put the stuff from a 2D array in

--- main.c
+++ main.c
@@ -6,16 +6,20 @@
 #include "fileops.h"
 #include <gtk/gtk.h>
 
-static void on_button_clicked(GtkButton *btn, gpointer data) {
-  gtk_button_set_label(btn, "You clicked me!");
-}
+static void activate(GtkApplication *app, gpointer data) {
+  GtkWidget *window;
+  GtkWidget *grid;
 
-static void on_app_activate(GApplication *app, gpointer data) {
-  GtkWidget *window = gtk_application_window_new(GTK_APPLICATION(app));
-  GtkWidget *btn = gtk_button_new_with_label("Hello World!");
-  g_signal_connect(btn, "clicked", G_CALLBACK(on_button_clicked), NULL);
-  gtk_container_add(GTK_CONTAINER(window), btn);
-  gtk_widget_show_all(GTK_WIDGET(window));
+  window = gtk_application_window_new(app);
+  gtk_window_set_title(GTK_WINDOW(window), "File Explorer");
+  gtk_window_set_default_size(GTK_WINDOW(window), 900, 650);
+
+  grid = gtk_grid_new();
+  GtkWidget *file = gtk_button_new_with_label("File Explorer");
+  gtk_grid_attach(GTK_GRID(grid), file, 1, 1, 1, 1);
+  gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(grid));
+
+  gtk_widget_show_all(window);
 }
 
 int main(int argc, char *argv[]) {
@@ -24,8 +28,9 @@ int main(int argc, char *argv[]) {
     G_APPLICATION_FLAGS_NONE
   );
 
-  g_signal_connect(app, "activate", G_CALLBACK(on_app_activate), NULL);
-  int status = g_application_run(G_APPLICATION(app), argc,argv);
+  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+  int status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
+
   return status;
 }
