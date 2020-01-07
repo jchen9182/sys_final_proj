02a233af7a3a06476babd56d1b328f68d5ad7751
Kevin Li <kevinli4321@gmail.com>
2020-01-06 22:50:40 -0500

    Icons show??? I guess???

--- main.c
+++ main.c
@@ -15,9 +15,22 @@ static void activate(GtkApplication *app, gpointer data) {
   gtk_window_set_default_size(GTK_WINDOW(window), 900, 650);
 
   grid = gtk_grid_new();
-  GtkWidget *file = gtk_button_new_with_label("File Explorer");
-  gtk_grid_attach(GTK_GRID(grid), file, 1, 1, 1, 1);
   gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(grid));
+  int num_files = 0;
+  char ** files = getfiles(&num_files);
+  GtkWidget *icon = gtk_image_new_from_file("finderico.png");
+
+  int i;
+  for (i = 0; i < 5; i++) {
+    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 30);
+    GtkWidget *iconbutton = gtk_button_new();
+    gtk_button_set_always_show_image(GTK_BUTTON(iconbutton), TRUE);
+    gtk_button_set_image(GTK_BUTTON(iconbutton), icon);
+    GtkWidget *filename = gtk_button_new_with_label(files[i]);
+    gtk_box_pack_start(GTK_BOX(box), iconbutton, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(box), filename, FALSE, FALSE, 0);
+    gtk_grid_attach(GTK_GRID(grid), box, i, i, 1, 1);
+  }
 
   gtk_widget_show_all(window);
 }
@@ -27,6 +40,16 @@ int main(int argc, char *argv[]) {
     "org.mks65.fileexp",
     G_APPLICATION_FLAGS_NONE
   );
+
+  GtkCssProvider *provider = gtk_css_provider_new();
+  /*
+  gtk_css_provider_load_from_path(provider, "style.css", NULL);
+  gtk_style_context_add_provider_for_screen(
+    gdk_screen_get_default(),
+    GTK_STYLE_PROVIDER(provider),
+    GTK_STYLE_PROVIDER_PRIORITY_USER
+  );
+  */
 
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
   int status = g_application_run(G_APPLICATION(app), argc, argv);
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 all: main
 
 main: main.c fileinfo.h execute.h
-	gcc `pkg-config --cflags gtk+-3.0` -o fileexp main.c `pkg-config --libs gtk+-3.0`
+	gcc `pkg-config --cflags gtk+-3.0` -o fileexp main.c fileinfo.c `pkg-config --libs gtk+-3.0`
 
 run:
 	./fileexp
