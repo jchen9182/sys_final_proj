bc8626869b70cb175816dd8fc3005af42380c1bd
Kevin Li <kevinli4321@gmail.com>
2020-01-13 20:23:45 -0500

    An actual grid lol

--- main.c
+++ main.c
@@ -16,20 +16,28 @@ static void activate(GtkApplication *app, gpointer data) {
 
   grid = gtk_grid_new();
   gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(grid));
+
   int num_files = 0;
   char ** files = getfiles(&num_files);
-  GtkWidget *icon = gtk_image_new_from_file("finderico.png");
-
+  //GtkWidget *icon = gtk_image_new_from_file("finderico.png");
   int i;
-  for (i = 0; i < 5; i++) {
-    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 30);
+  int row, col = 0;
+  for (i = 0; i < num_files; i++) {
+    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     GtkWidget *iconbutton = gtk_button_new();
+    GtkWidget *iconimg = gtk_image_new_from_file("finderico.png");
     gtk_button_set_always_show_image(GTK_BUTTON(iconbutton), TRUE);
-    gtk_button_set_image(GTK_BUTTON(iconbutton), icon);
-    GtkWidget *filename = gtk_button_new_with_label(files[i]);
+    gtk_button_set_image(GTK_BUTTON(iconbutton), iconimg);
+    GtkWidget *filename = gtk_label_new(files[i]);
     gtk_box_pack_start(GTK_BOX(box), iconbutton, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(box), filename, FALSE, FALSE, 0);
-    gtk_grid_attach(GTK_GRID(grid), box, i, i, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), box, col, row, 1, 1);
+
+    col++;
+    if (col >= 10) {
+      row++;
+      col = 0;
+    }
   }
 
   gtk_widget_show_all(window);
@@ -40,16 +48,6 @@ int main(int argc, char *argv[]) {
     "org.mks65.fileexp",
     G_APPLICATION_FLAGS_NONE
   );
-
-  GtkCssProvider *provider = gtk_css_provider_new();
-  /*
-  gtk_css_provider_load_from_path(provider, "style.css", NULL);
-  gtk_style_context_add_provider_for_screen(
-    gdk_screen_get_default(),
-    GTK_STYLE_PROVIDER(provider),
-    GTK_STYLE_PROVIDER_PRIORITY_USER
-  );
-  */
 
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
   int status = g_application_run(G_APPLICATION(app), argc, argv);
