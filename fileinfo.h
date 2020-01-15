c610ef8dcac4a38a20f38d96289fb24b9bc5e879
SamuelFang <47552568+SamuelFang@users.noreply.github.com>
2020-01-15 11:00:56 -0500

    Update fileinfo.h

--- fileinfo.h
+++ fileinfo.h
@@ -5,3 +5,7 @@ char ** getfiles(int *num_files);
 void get_ext(char *filename, char *buff);
 
 void get_props(char *filename, struct fileprops * props);
+
+char * get_properties(char *file_name);
+
+char * get_dir();
